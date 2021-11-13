91438c1e632f8da3f577852047dcc99e6e34bce1
Furkan Koç <93766996+frknkoc@users.noreply.github.com>
2021-11-13 14:45:59 +0300

    Create task9.sql

---
+++ task9.sql
@@ -0,0 +1,6 @@
+SELECT city, country FROM city INNER JOIN cıty ON city = country;
+
+SELECT customer.first_name, customer.last_name, payment FROM customer INNER JOIN payment.payment_id ON city = customer.customer.id;
+
+SELECT customer.first_name, customer.last_name, rental.rental_id FROM customer INNER JOIN rental.rental_id ON city = customer.customer.id;
+
